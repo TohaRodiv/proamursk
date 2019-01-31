adeefd5f80afd44031432af820675daed3a1caab
Виктор Кондик <strunoder24@mail.ru>
2019-01-31 18:56:58 +1000

    Правки для vimeo

--- v-settings/components/forms/tab/widgets/postEditor/postEditorWidgetViews/Video.vue
+++ v-settings/components/forms/tab/widgets/postEditor/postEditorWidgetViews/Video.vue
@@ -1,12 +1,25 @@
 <template>
-    <iframe
-            width="656"
-            height="315"
-            :src="handleYoutubeLinkFormat()"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen>
-    </iframe>
+    <div>
+        <iframe
+                v-if="code.indexOf('youtube.com') !== -1"
+                width="656"
+                height="360"
+                :src="handleYoutubeLinkFormat()"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+        </iframe>
+        <iframe
+                v-if="code.indexOf('vimeo.com') !== -1"
+                :src="handleVimeoLinkFormat()"
+                width="656"
+                height="360"
+                frameborder="0"
+                webkitallowfullscreen
+                mozallowfullscreen
+                allowfullscreen>
+        </iframe>
+    </div>
 </template>
 
 <script>
@@ -21,15 +34,21 @@
                 let withoutWatch = link.split('watch?v=');
                 withoutWatch[0] = withoutWatch[0] + 'embed/';
                 link = withoutWatch.join('');
-                if (link.indexOf('&t')) {
+                if (link.indexOf('&t') !== -1) {
                     let withoutTime = link.split('&t');
                     link = withoutTime[0];
                 }
-                if (link.indexOf('?t')) {
+                if (link.indexOf('?t') !== -1) {
                     let withoutTime = link.split('?t');
                     link = withoutTime[0];
                 }
                 return link
+            },
+
+            handleVimeoLinkFormat(){
+                let base = 'https://player.vimeo.com/video/';
+                let link = this.code.split('/');
+                return base + link[link.length - 1];
             },
         },
     }
