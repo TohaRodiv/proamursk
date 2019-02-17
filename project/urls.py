6bc4e5dcb08c338e7678ec48baff6256226b9a2a
Vitaly Yurusov <v.yurusov@gmail.com>
2019-02-17 19:33:06 +1000

    Внес правки

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,7 @@ from applications.root import views as root_views
 from applications.sitesettings.views import RobotsTxtView
 
 urlpatterns = [
-    path('admin/', TemplateView.as_view(template_name="cp_vue/index.html")),
+    re_path(r'^admin/', TemplateView.as_view(template_name="cp_vue/index.html")),
     path('api/v1/', include(cp_api.urls)),
     path('',  root_views.IndexView.as_view(), name='index'),
     path('news/',  root_views.NewsListView.as_view(), name='news-list'),
