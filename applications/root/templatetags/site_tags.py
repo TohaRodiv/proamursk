6f91ac5d3ce2fabdf31dd0372292c1b1e9b310aa
Vitaly Yurusov <yurusov@gmail.com>
2019-02-18 17:24:27 +1000

    Сделал вывод банеров

--- applications/root/templatetags/site_tags.py
+++ applications/root/templatetags/site_tags.py
@@ -1,13 +1,64 @@
 # -*- coding: utf-8 -*-
 
+from django.db.models import Q
 from django.template import Library
 from datetime import datetime
 from django.conf import settings
-from applications.root.models import News, Person, History
+from applications.root.models import News, Person, History, SidebarBanner
 from applications.contentblocks.models import Page
 
 register = Library()
 
+
+@register.inclusion_tag('site/page-block/sidebar-banner.html', takes_context=True)
+def get_banner(context):
+    banners = SidebarBanner.objects.filter((Q(start_publication_date__isnull=True) | Q(start_publication_date__lte=datetime.now())) &
+                                           (Q(end_publication_date__isnull=True) | Q(end_publication_date__gte=datetime.now())),
+                                           is_active=True)
+
+    request = context.get('request')
+    show_two_banners = False
+    if request:
+        try:
+            url_name = request.resolver_match.url_name
+        except:
+            pass
+        else:
+            if url_name == 'news-detail':
+                item = context.get('news')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'events-detail':
+                item = context.get('event')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'reports-detail':
+                item = context.get('report')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'persons-detail':
+                item = context.get('person')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'history-detail':
+                item = context.get('history')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'city-guides-detail':
+                item = context.get('guide')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'places-detail':
+                item = context.get('place')
+                show_two_banners = item.show_two_banners
+            elif url_name == 'films-detail':
+                item = context.get('film')
+                show_two_banners = item.show_two_banners
+
+    banners = banners[:2] if show_two_banners else banners[:1]
+    return dict(banners=banners, show_two_banners=show_two_banners)
+
+
+@register.filter()
+def get_model_name(val):
+    try:
+        return val._meta.model_name
+    except:
+        return None
 
 @register.inclusion_tag('site/page-block/last-news-widget.html', takes_context=True)
 def last_news(context):
