0a91c0404c19920a5b4bcfcc2e657fc1c6295e62
kinyashov <kinyashov@gmail.com>
2019-02-25 17:22:58 +1000

    Поправил настройку вэбпака, чтобы корректно собирался билд

--- v-settings/webpack/base.config.js
+++ v-settings/webpack/base.config.js
@@ -15,6 +15,15 @@ if (process.env.NODE_ENV === 'development') {
                     test: /\.js$/,
                     use: {
                         loader: 'babel-loader',
+                        options: {
+                            'presets': [
+                                'es2015',
+                                'stage-0',
+                                'stage-2'
+                            ],
+                            comments: false,
+                            'babelrc': false
+                        },
                     },
                     exclude: /node_modules/
                 },
