b05e34ce17cfcbb8d41503186beccf5e0369b372
Vitaly Yurusov <yurusov@gmail.com>
2019-02-26 11:03:00 +1000

    Убрал коментарий

--- project/settings.py
+++ project/settings.py
@@ -141,10 +141,6 @@ USE_I18N = True
 
 USE_L10N = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
