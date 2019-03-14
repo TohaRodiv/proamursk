3b0f15a1f7a4355284f874069d9b9ea8eabc6267
kinyashov <kinyashov@gmail.com>
2019-03-14 13:45:43 +1000

    Добавил текст в подсказку виджета рассылок

--- v-settings/configs/emailing/forms/campaigns.js
+++ v-settings/configs/emailing/forms/campaigns.js
@@ -21,6 +21,48 @@ const hint = `<span style="font-weight: 600;">Тег для публикаций
     <li>
         <span>{% separator  %} — Сепаратор.</span>
     </li>
+</ul>
+<span style="font-weight: 600;">Типы публикаций:</span>
+<ul>
+    <li>
+        <span>new — Новость</span>
+    </li>
+    <li>
+        <span>event — Анонс события</span>
+    </li>
+    <li>
+        <span>report — Репортаж о прошедшем событии</span>
+    </li>
+    <li>
+        <span>history — Историческая статья</span>
+    </li>
+    <li>
+        <span>person — Статья о жителе Амурска</span>
+    </li>
+    <li>
+        <span>place — Статья о месте</span>
+    </li>
+    <li>
+        <span>guide — Гид по городу</span>
+    </li>
+    <li>
+        <span>special — Спецпроект</span>
+    </li>
+</ul>
+<span style="font-weight: 600;">Форматы публикаций:</span>
+<ul>
+    <li>
+        <span>wf — Широкая (wide) с полноразмерной (full) обложкой</span>
+    </li>
+    <li>
+        <span>ws — Широкая (wide) с обычной (standart) обложкой</span>
+    </li>
+    <li>
+        <span>hf — В половину ширины (half) с полноразмерной (full) обложкой</span>
+    </li>
+    <li>
+        <span>hs — В половину ширины (half) с обычной (standart) обложкой</span>
+    </li>
 </ul>`
 
 const state = {
