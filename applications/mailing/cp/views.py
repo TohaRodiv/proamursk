73ae510eaef25fed583a6cae1b311c5969e442f4
Vitaly Yurusov <yurusov@gmail.com>
2019-03-14 17:28:15 +1000

    Поправил email для рассылок

--- applications/mailing/cp/views.py
+++ applications/mailing/cp/views.py
@@ -94,7 +94,9 @@ class CampaignsCpViewSet(CpViewSet):
                 text = self.render_string(self.linebreaksbr(serializer.data.get('template', '')), dict())
                 email = serializer.data.get('email')
                 template = get_template("notifications/email/email.html")
-                context = dict(text=text, domain=settings.ROOT_LINK if hasattr(settings, 'ROOT_LINK') else '')
+                context = dict(text=text,
+                               unsubscribe=True,
+                               domain=settings.ROOT_LINK if hasattr(settings, 'ROOT_LINK') else '')
                 content = template.render(context)
                 subject = 'Тест рассылки'
             except Exception as e:
