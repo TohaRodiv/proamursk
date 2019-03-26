172412b76400ade365ee3a84e5c76dc1d8e06f3d
kinyashov <kinyashov@gmail.com>
2019-03-26 19:17:30 +1000

    Добавил сайдбар для форм

--- v-settings/components/forms/Forms.vue
+++ v-settings/components/forms/Forms.vue
@@ -1,17 +1,6 @@
 <template>
     <div class="forms">
-        <div class="forms-tabs-container" v-if=" (config) ? config.length > 1 && formsCurrentMode !== 'moving' : false">
-            <div class="forms-tabs-item"
-                 :class="{'forms-tabs-item-error': tab.invalid, 'forms-tabs-item-selected': tab.id === currentTabId}"
-                 v-show="(tab.renderFlag || tab.forbiddenFlag) ? checkTriggersFlag(tab) : true"
-                 v-if="!(tab.cancelRenderIFSuperuser && getIsSuperUser && itsMineUserForm())"
-                 v-for="(tab, tabDex) in config"
-                 :key="tabDex"
-                 @click="setCurrentTab(tab.id)">
-                <span>{{ tab.title }}</span>
-                <div v-if="tab.id === currentTabId" class="forms-tabs-item-selected-border"></div>
-            </div>
-        </div>
+        
         <div class="forms-sidebar-container" v-if="formsCurrentMode === 'moving'">
             <div class="forms-sidebar-header">
                 <div class="forms-sidebar-header-small-text">Движение по статусам</div>
@@ -78,9 +67,23 @@
         <div class="forms-wrapper">
             <!--Левый бордер ниже необходим, так как контейнер форм на всю длину страницы (чтобы подсказки заходили на сайдбар), и не могут отрисовать правый бордер-->
             <div class="forms-left-border"></div>
-            <div class="forms-wrapper-inner" :class="{'width100percents': !getMovingData || !getMovingData.status_log}">
-                <div class="forms-workzone" :class="{'forms-workzone-no-tabs': config.length === 1 || formsCurrentMode === 'moving'}">
-                    <tab
+            <div class="tabs-wrapper">
+                <div class="forms-tabs-container" v-if=" (config) ? config.length > 1 && formsCurrentMode !== 'moving' : false">
+                    <div class="forms-tabs-item"
+                        :class="{'forms-tabs-item-error': tab.invalid, 'forms-tabs-item-selected': tab.id === currentTabId}"
+                        v-show="(tab.renderFlag || tab.forbiddenFlag) ? checkTriggersFlag(tab) : true"
+                        v-if="!(tab.cancelRenderIFSuperuser && getIsSuperUser && itsMineUserForm())"
+                        v-for="(tab, tabDex) in config"
+                        :key="tabDex"
+                        @click="setCurrentTab(tab.id)"
+                    >
+                        <span>{{ tab.title }}</span>
+                        <div v-if="tab.id === currentTabId" class="forms-tabs-item-selected-border"></div>
+                    </div>
+                </div>
+                <div class="forms-wrapper-inner" :class="{'width100percents': !getMovingData || !getMovingData.status_log}">
+                    <div class="forms-workzone" :class="{'forms-workzone-no-tabs': config.length === 1 || formsCurrentMode === 'moving'}">
+                        <tab
                             :data="data"
                             v-for="(tab, index) in config"
                             :key="tab.id"
@@ -88,9 +91,17 @@
                             :tabIndex="index"
                             :reopenTrigger="reopenTrigger"
                             v-show="(formsCurrentMode !== 'moving') ? tab.id === currentTabId : ((newMode) ? tab.id === 1 : true)">
-                    </tab>
+                        </tab>
+                    </div>
                 </div>
             </div>
+            <formSidebar
+                v-if="computedShowFormSidebar"
+                :propIsAddForm="computedIsAddForm"
+                :propIsEditForm="computedIsEditForm"
+                :propShowActivator="computedShowActivator"
+                :propActiveFlagConfig="computedActiveFlagConfig"
+            ></formSidebar>
         </div>
 
         <deletePopup @closeDeletePopup="showDeletePopup = false" v-if="showDeletePopup" @onDelete="typeOfMove === 'onDelete'"> </deletePopup>
@@ -98,7 +109,6 @@
         <infoPopup @closeInfoPopup="showInfoPopup = false" @infoPopupResume="saveEntity('moveForward')" v-if="showInfoPopup"> </infoPopup>
         <unsavedPopup @closePopup="showUnsavedPopup = false" @continueConfirmed="continueConfirmed = true" :pathForRouter="unsavedPopupDaWae" v-if="showUnsavedPopup"> </unsavedPopup>
         <historyPopup @closePopup="showHistoryPopup = false" v-if="showHistoryPopup"> </historyPopup>
-
         <div v-if="showAdditionalPreloaderData">
             <div class="forms-buttons-container" v-if="!isNaN(+($route.params.id))">
                 <div class="forms-buttons-container-inner">
@@ -113,15 +123,15 @@
                     <button v-if="getIsSuperUser || (data.object_permissions && data.object_permissions.edit)" class="button forms-save-button" @click="saveEntity('save')">Сохранить</button>
                 </div>
             </div>
-
-            <div class="forms-buttons-container" v-if="isNaN(+($route.params.id))">
+            <div class="forms-buttons-container" v-if="isNaN($route.params.id)">
                 <div class="forms-buttons-container-inner">
-                    <button class="button borderless-button forms-save-and-add-button" v-if="$route.params.id !== 'form' && (getIsSuperUser || hasAddRight())" @click="saveEntity('add&push')">Сохранить и добавить</button>
+                    <button class="button borderless-button forms-save-and-add-button" v-if="$route.params.id !== 'form' && (getIsSuperUser || (hasAddRight() && hasAddButton))" @click="saveEntity('add&push')">Сохранить и добавить</button>
                     <button v-if="getIsSuperUser || hasAddRight()" class="button forms-save-button" @click="($route.params.id === 'form') ? saveEntity('save') : saveEntity('add')">Сохранить</button>
                 </div>
             </div>
 
         </div>
+        
     </div>
 </template>
 
