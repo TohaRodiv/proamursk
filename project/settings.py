fcad7481782a263c363eedc9a29b671093cd120e
Валентина Видяева <vvidyaeva@gmail.com>
2019-06-26 18:04:26 +1000

    пвс88

--- project/settings.py
+++ project/settings.py
@@ -149,7 +149,7 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
-STATIC_VERSION = 87
+STATIC_VERSION = 88
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
