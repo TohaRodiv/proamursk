c7a04a8a0c932024669769cfdbeb7096d1f15d97
kinyashov <kinyashov@gmail.com>
2019-10-29 16:00:19 +1000

    ПВС №146

--- v-settings/configs/publications/controllers/reports.js
+++ v-settings/configs/publications/controllers/reports.js
@@ -1,13 +1,18 @@
 export default {
     change: {
-        event({ event_date_text, place, coordinates, }, { setNewValues, }) {
-            if (event_date_text && place && coordinates) {
-                const item = {
-                    event_date_text,
-                    place,
-                    coordinates,
-                };
-                setNewValues(item);
+        event(event, { setNewValues, }) {
+            if (event) {
+                const event_date_text = event.event_date_text;
+                const place = event.place;
+                const coordinates = event.coordinates;
+                if (event_date_text && place && coordinates) {
+                    const item = {
+                        event_date_text,
+                        place,
+                        coordinates,
+                    };
+                    setNewValues(item);
+                }
             }
         },
     },
