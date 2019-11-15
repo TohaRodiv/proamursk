7fab7a718856e76420433cc0cf993e957b6b9306
Валентина Видяева <vvidyaeva@gmail.com>
2019-11-15 18:23:19 +1000

    Правки по тексту

--- frontend/src/js/nav-generator.js
+++ frontend/src/js/nav-generator.js
@@ -9,19 +9,19 @@ $(function() {
             append: $('<a/>', {
                 class: 'privacy__link',
                 text: currentItemText,
-                href: '#section' + (index + 1)
-            })
+                href: '#section' + (index + 1),
+            }),
         }).appendTo('.js-privacy-nav');
-    })
-})
+    });
+});
 
 $(document).ready(function(){
     $('a[href*="#"]').on("click", function(e){
         var anchor = $(this),
             fixedTopOffset = 40;
         $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top - fixedTopOffset
-        }, 300);
+            scrollTop: $(anchor.attr('href')).offset().top - fixedTopOffset,
+        }, 500);
         e.preventDefault();
         return false;
     });
--- static/site/js/main.js
+++ static/site/js/main.js
@@ -7509,7 +7509,7 @@ $(document).ready(function () {
         fixedTopOffset = 40;
     $('html, body').stop().animate({
       scrollTop: $(anchor.attr('href')).offset().top - fixedTopOffset
-    }, 300);
+    }, 500);
     e.preventDefault();
     return false;
   });
