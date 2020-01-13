943edabab5e9fd221e96b2be702547e7b0c85641
kinyashov <kinyashov@gmail.com>
2020-01-13 12:28:37 +1000

    🔧 Подстановка api в подборке

--- v-settings/configs/publications/controllers/items.js
+++ v-settings/configs/publications/controllers/items.js
@@ -1,7 +1,16 @@
+const apis = {
+    event: 'event-announcements',
+    news: 'news',
+    report: 'reports',
+    history: 'history',
+    place: 'places',
+    person: 'persons',
+};
+
 export default {
     change: {
         entity(entity, { fields, formData, }) {
-            fields.object_data.api = entity;
+            fields.object_data.api = apis[entity];
             formData.object_data = null;
             formData.object_id = null;
         },
