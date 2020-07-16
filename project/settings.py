92ff1e8379ba3adcc88be94b31544082f95680d6
kinyashov <kinyashov@gmail.com>
2020-07-16 11:34:50 +1000

    🎬 197

--- project/settings.py
+++ project/settings.py
@@ -149,7 +149,7 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
-STATIC_VERSION = 196
+STATIC_VERSION = 197
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
