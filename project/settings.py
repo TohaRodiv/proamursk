1a90b1ca60f702e50d281f3455eb32cc22dacf9b
Valya Vidyaeva <vvidyaeva@gmail.com>
2021-02-23 17:25:34 +0300

    пвс210

--- project/settings.py
+++ project/settings.py
@@ -149,7 +149,7 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
-STATIC_VERSION = 209
+STATIC_VERSION = 210
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
